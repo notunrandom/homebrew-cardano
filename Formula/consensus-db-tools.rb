8851d85c080ea2591172bd34de7c9c7c5dafb904
notunrandom <122674938+notunrandom@users.noreply.github.com>
2025-12-11 16:15:10 +0000

    consensus-db-tools: add 020 bottle.

--- Formula/consensus-db-tools.rb
+++ Formula/consensus-db-tools.rb
@@ -5,6 +5,14 @@ class ConsensusDbTools < Formula
   sha256 "53428c5e8c62807dbaf97a9f201b702de2dc942e8b81661482d0a3c5f8d69994"
   license "Apache-2.0"
 
+  bottle do
+    root_url "https://github.com/notunrandom/homebrew-cardano/releases/download/consensus-db-tools-020"
+    sha256 cellar: :any,                 arm64_tahoe:   "0f773665f2508de9b409a22a307c10f639787fb57328bdcab2774406ef51ac9b"
+    sha256 cellar: :any,                 arm64_sequoia: "25425237653a853531c24f3270309446df98855c97bc1f6bd488aabbb6a0a566"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "711c67e51c5a931c88b514a8ea478e18ee589a236ba9c600e1bf3c3483689f61"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "e4cb74ade1cd3c153a2946e242e78db0ffd297f04b45d2b93716d54726ba3b69"
+  end
+
   depends_on "ghcup" => :build
   depends_on "pkg-config" => :build
   depends_on "gmp"
