2e1e0390f39dde729b0b92edf502aa937da67796
notunrandom <122674938+notunrandom@users.noreply.github.com>
2025-12-11 09:00:00 +0100

    New formula: secp256k1@0.3.2

---
+++ Formula/secp256k1@0.3.2.rb
@@ -0,0 +1,40 @@
+class Secp256k1AT032 < Formula
+  desc "Optimized C library for EC operations on curve secp256k1"
+  homepage "https://github.com/bitcoin-core/secp256k1"
+  url "https://github.com/bitcoin-core/secp256k1/archive/refs/tags/v0.3.2.tar.gz"
+  sha256 "ef2e1061951b8cf94a7597b4e60fd7810613e327e25305e8d73dfdff67d12190"
+  license "MIT"
+
+  keg_only :versioned_formula
+
+  depends_on "autoconf" => [:build]
+  depends_on "automake" => [:build]
+  depends_on "libtool" => [:build]
+
+  def install
+    system "./autogen.sh"
+    args = %w[
+      --disable-silent-rules
+      --enable-module-recovery
+      --enable-module-ecdh
+      --enable-module-schnorrsig
+      --enable-module-extrakeys
+    ]
+    system "./configure", *args, *std_configure_args
+    system "make"
+    system "make", "install"
+  end
+
+  test do
+    (testpath/"test.c").write <<~C
+      #include <secp256k1.h>
+      int main() {
+        secp256k1_context* ctx = secp256k1_context_create(SECP256K1_CONTEXT_NONE);
+        secp256k1_context_destroy(ctx);
+        return 0;
+      }
+    C
+    system ENV.cc, "test.c", "-I#{include}", "-L#{lib}", "-lsecp256k1", "-o", "test"
+    system "./test"
+  end
+end
