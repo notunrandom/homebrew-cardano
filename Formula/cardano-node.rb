d2f4b6310237d3391aca757326f4eb65fb9387c5
notunrandom <122674938+notunrandom@users.noreply.github.com>
2026-05-05 10:22:00 +0200

    Update cardano-node formula to 10.7.1

--- Formula/cardano-node.rb
+++ Formula/cardano-node.rb
@@ -1,18 +1,9 @@
 class CardanoNode < Formula
   desc "Core component needed to participate in a Cardano decentralised blockchain"
   homepage "https://github.com/IntersectMBO/cardano-node"
-  url "https://github.com/IntersectMBO/cardano-node/archive/refs/tags/10.6.2.tar.gz"
-  sha256 "a68e36f5d06ffb999c1e74135f3e97185cc17077b4e055bdaab20a7af56f144e"
+  url "https://github.com/IntersectMBO/cardano-node/archive/refs/tags/10.7.1.tar.gz"
+  sha256 "57749818645c0b2efe7e5ac1a97078452dc2ebd7620d75c30accf8737f247ce3"
   license "Apache-2.0"
-
-  bottle do
-    root_url "https://github.com/notunrandom/homebrew-cardano/releases/download/cardano-node-10.6.2"
-    rebuild 3
-    sha256 cellar: :any,                 arm64_tahoe:   "dbd3eb9091b453c1b7a59689b7c0d085f4360c8a37243f6b68695166ab4379b5"
-    sha256 cellar: :any,                 arm64_sequoia: "339ee4f1912d7f444c5f087167bf5712713931d06989ca0f846032ddc06716fe"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "8788c1ecb41df28d83a5b3caef79567f55c55b5f8c31c2a3774357a9e603606a"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "ccf6d73bd1aa56d47b05bbb3f6f3981df1600e346cf47cf2972eb2845d895a49"
-  end
 
   depends_on "cabal-install" => :build
   depends_on "ghc@9.6" => :build
@@ -24,6 +15,8 @@ class CardanoNode < Formula
   depends_on "notunrandom/cardano/secp256k1@0.3.2"
   depends_on "openssl"
   depends_on "pkgconf"
+  depends_on "protobuf"
+  depends_on "snappy"
 
   on_linux do
     depends_on "ncurses"
@@ -39,34 +32,49 @@ class CardanoNode < Formula
       system "cabal", "v2-install", *std_cabal_v2_args, tool
     end
     %w[mainnet preprod preview].each do |network|
-      (var/"cardano"/network/"db").mkpath
+      (var/"cardano/#{version}/#{network}/db").mkpath
     end
-    symlink_etc = etc/"cardano"/"network"
-    ln_s etc/"cardano"/"mainnet", symlink_etc unless File.exist?(symlink_etc)
-    symlink_var = var/"cardano"/"network"
-    ln_s var/"cardano"/"mainnet", symlink_var unless File.exist?(symlink_var)
+    (libexec/"cardano-node-service.sh").write <<~SH
+      #!/bin/bash
+      NETWORK=preprod
+      PORT=3001
+      SVCCONFIG="#{etc}/cardano/#{version}/cardano-node-service.conf"
+      if [[ -f "$SVCCONFIG" ]]; then
+        source "$SVCCONFIG"
+      fi
+      CONFIG="#{etc}/cardano/#{version}/$NETWORK/config.json"
+      TOPOLOGY="#{etc}/cardano/#{version}/$NETWORK/topology.json"
+      DB="#{var}/cardano/#{version}/$NETWORK/db"
+      SOCKET="#{var}/cardano/#{version}/$NETWORK/node.socket"
+      exec "#{bin}/cardano-node" run --config $CONFIG --topology $TOPOLOGY --database-path $DB --socket-path $SOCKET --port $PORT
+    SH
+    (libexec/"cardano-node-service.sh").chmod 0755
   end
 
   service do
-    run [
-      HOMEBREW_PREFIX/"opt"/"cardano-node"/"bin"/"cardano-node",
-      "run",
-      "--config", etc/"cardano"/"network"/"config.json",
-      "--topology", etc/"cardano"/"network"/"topology.json",
-      "--database-path", var/"cardano"/"network"/"db",
-      "--socket-path", var/"cardano"/"network"/"node.socket",
-      "--port", "3001"
-    ]
+    run [opt_libexec/"cardano-node-service.sh"]
     keep_alive true
-    log_path var/"cardano"/"network"/"log"
-    error_log_path var/"cardano"/"network"/"log"
+    log_path var/"cardano/log"
+    error_log_path var/"cardano/log"
+  end
+
+  def caveats
+    <<~EOS
+      By default the service will run a preprod node.
+      To change this or other parameters see:
+      https://github.com/notunrandom/homebrew-cardano
+    EOS
   end
 
   test do
     system bin/"cardano-node", "--version"
-    assert_path_exists var/"cardano"/"preprod"/"db"
-    assert_path_exists var/"cardano"/"network"
-    assert_path_exists etc/"cardano"/"network"
+    system bin/"cardano-cli", "--version"
+    assert_path_exists var/"cardano/#{version}/preview/db"
+    assert_path_exists var/"cardano/#{version}/preprod/db"
+    assert_path_exists var/"cardano/#{version}/mainnet/db"
+    assert_path_exists etc/"cardano/#{version}/preview/config.json"
+    assert_path_exists etc/"cardano/#{version}/preprod/config.json"
+    assert_path_exists etc/"cardano/#{version}/mainnet/config.json"
     system "brew", "services", "info", "notunrandom/cardano/cardano-node"
   end
 end
