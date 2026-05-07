df5446b55de3cc9917ff0c827c1cf392ac88b5f0
notunrandom <122674938+notunrandom@users.noreply.github.com>
2026-05-07 09:16:13 +0000

    kupo: add 2.11 bottle.

--- Formula/kupo.rb
+++ Formula/kupo.rb
@@ -5,6 +5,15 @@ class Kupo < Formula
   sha256 "1b823b35c31b46ffce2794eea570dd6ace826b171a839d6d57d72780ec646a78"
   license "MPL-2.0"
 
+  bottle do
+    root_url "https://github.com/notunrandom/homebrew-cardano/releases/download/kupo-2.11"
+    rebuild 1
+    sha256 cellar: :any,                 arm64_tahoe:   "9c4c0e1d7067bd65a596814bee23055f186858ca25260ad2b6659c1e74d1245e"
+    sha256 cellar: :any,                 arm64_sequoia: "cce62204431b65f10b1ad075a6812750acb119bd9cd5c985be52b6cdd9a280b1"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "a76745124e08960966ae775165ad3d3fb5faeea5ab3f985dcfeb394ad22e9ddc"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "1634265cb61031c4abcd691364d78305c5693d7683eb08b49143df9109c8bcda"
+  end
+
   depends_on "cabal-install" => :build
   depends_on "ghc@9.6" => :build
   depends_on "hpack" => :build
