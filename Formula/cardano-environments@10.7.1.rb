1eb11f4348745f0c4f45807900d633747f51655c
notunrandom <122674938+notunrandom@users.noreply.github.com>
2026-05-12 13:47:43 +0200

    Versioned (10.7.1) cardano-environments
    Closes #23.
    
    Signed-off-by: notunrandom <122674938+notunrandom@users.noreply.github.com>

---
+++ Formula/cardano-environments@10.7.1.rb
@@ -0,0 +1,31 @@
+class CardanoEnvironmentsAT1071 < Formula
+  desc "Versioned copy of configuration files for Cardano networks"
+  homepage "https://github.com/notunrandom/cardano-environments"
+  url "https://github.com/notunrandom/cardano-environments/archive/refs/tags/10.7.1.tar.gz"
+  sha256 "c323f43db37de9a85d9991bb52b8c9ee6a58549644d95a6f062269fde49e566c"
+  license "Apache-2.0"
+
+  def install
+    (prefix/"placeholder").write("")
+    (etc/"cardano/#{version}/mainnet").mkpath
+    (etc/"cardano/#{version}/preprod").mkpath
+    (etc/"cardano/#{version}/preview").mkpath
+    (etc/"cardano/#{version}/mainnet").install Dir["mainnet/*"]
+    (etc/"cardano/#{version}/preprod").install Dir["preprod/*"]
+    (etc/"cardano/#{version}/preview").install Dir["preview/*"]
+  end
+
+  def caveats
+    <<~EOS
+      Cardano environments have been installed to #{etc}/cardano/#{version}.
+      To use, e.g. for mainnet, pass this flag to cardano-node:
+        --config $(brew --prefix)/etc/cardano/#{version}/mainnet/config.json
+    EOS
+  end
+
+  test do
+    assert_path_exists etc/"cardano/#{version}/mainnet/config.json"
+    assert_path_exists etc/"cardano/#{version}/preprod/config.json"
+    assert_path_exists etc/"cardano/#{version}/preview/config.json"
+  end
+end
