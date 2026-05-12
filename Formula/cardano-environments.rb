348a85727c832e881a757154dcecea60a226e501
notunrandom <122674938+notunrandom@users.noreply.github.com>
2026-05-12 14:03:43 +0000

    cardano-environments: add 11.0.1 bottle.

--- Formula/cardano-environments.rb
+++ Formula/cardano-environments.rb
@@ -5,6 +5,14 @@ class CardanoEnvironments < Formula
   sha256 "e920d38a6f4e2b23e591040530e515e770075f3cb4ce2e51e7eafcb1082bf7ae"
   license "Apache-2.0"
 
+  bottle do
+    root_url "https://github.com/notunrandom/homebrew-cardano/releases/download/cardano-environments-11.0.1"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "ddaecf8d60ad8badb4bb9685e649268587828cac4d72795d80b4a63d73dc5161"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "4b7442951ec1e81edec9a9040d4a318ff95aed7cfbff99b89d729b37d75884e8"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "6344fe371666ea5bdef98931d54c744f912737776fcb2299af2f945e3ff22bfd"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "327d076e43d57721619541ec59a562c52b45a84169497fb5966e6d19aeb8f38f"
+  end
+
   def install
     (prefix/"placeholder").write("")
     (etc/"cardano/#{version}/mainnet").mkpath
