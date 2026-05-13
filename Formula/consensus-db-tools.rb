9f838ca7a5e9908f54e5f6b08771afa0ba4216cb
notunrandom <122674938+notunrandom@users.noreply.github.com>
2026-05-13 13:01:44 +0000

    consensus-db-tools: add 11.0.1 bottle.

--- Formula/consensus-db-tools.rb
+++ Formula/consensus-db-tools.rb
@@ -7,6 +7,14 @@ class ConsensusDbTools < Formula
   license "Apache-2.0"
   version_scheme 1
 
+  bottle do
+    root_url "https://github.com/notunrandom/homebrew-cardano/releases/download/consensus-db-tools-11.0.1"
+    sha256 cellar: :any,                 arm64_tahoe:   "bfa0b20072860ca94cb8623af4c57431bc8e9d4a44085b998a2d6bd77b73a4ab"
+    sha256 cellar: :any,                 arm64_sequoia: "c384f09b08ea1b7f77ab05165cee9f8d4c7e9f3d8a3e19f58219f3544318254d"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "b58bb2d4a0ba4cbc29c65a3e79009bd691c2ac9c67afbb9181ad8640a773b07b"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "db526417fbd9cfa266e9b9474879f02778d53ec7dc37b73a89cd3449b1430dfb"
+  end
+
   depends_on "ghcup" => :build
   depends_on "pkg-config" => :build
   depends_on "gmp"
