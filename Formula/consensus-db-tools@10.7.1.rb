90cc556f4d2401e137dae0faab50d87db83b4103
notunrandom <122674938+notunrandom@users.noreply.github.com>
2026-07-21 16:10:34 +0200

    Satisfy new brew style lint rule

--- Formula/consensus-db-tools@10.7.1.rb
+++ Formula/consensus-db-tools@10.7.1.rb
@@ -33,7 +33,7 @@ class ConsensusDbToolsAT1071 < Formula
     system "ghcup", "install", "ghc", "9.6.7"
     system "ghcup", "install", "cabal", "3.12.1.0"
     ENV.prepend_path "PATH", buildpath/".ghcup/bin"
-    ENV.prepend_path "PKG_CONFIG_PATH", Formula["notunrandom/cardano/secp256k1@0.3.2"].opt_lib/"pkgconf"
+    ENV.prepend_path "PKG_CONFIG_PATH", secp256k1.opt_lib/"pkgconf"
     File.write("cabal.project.local", "with-compiler: ghc-9.6.7")
     system "cabal", "update"
     system "cabal", "build", "exe:db-analyser"
