95e64d59468b6853df2da0198fbe05e762332749
notunrandom <122674938+notunrandom@users.noreply.github.com>
2026-08-04 06:54:19 +0000

    ogmios: add 7.0.0 bottle.

--- Formula/ogmios.rb
+++ Formula/ogmios.rb
@@ -5,6 +5,14 @@ class Ogmios < Formula
   version "7.0.0"
   license "MPL-2.0"
 
+  bottle do
+    root_url "https://github.com/notunrandom/homebrew-cardano/releases/download/ogmios-7.0.0"
+    sha256 cellar: :any, arm64_tahoe:   "85e31b80ea0d23ef608614e9bacc4af4517beeaffa4faea5bf1b905c252aa62a"
+    sha256 cellar: :any, arm64_sequoia: "81317b1d4a29a3494fa3bdc64b0a6c4fdb43bf296924c7b72c459d26a668cdf4"
+    sha256 cellar: :any, arm64_linux:   "473f228f526904e322d75c89195e78b24f27b7b7f9a57f65a74081821fe97099"
+    sha256 cellar: :any, x86_64_linux:  "14708e583eb34e31be7039584f811cba9ec9b4362ae49b997997a7cc8fab4448"
+  end
+
   depends_on "cabal-install" => :build
   depends_on "ghc@9.6" => :build
   depends_on "notunrandom/cardano/blst"
