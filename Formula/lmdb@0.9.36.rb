d7ec55468bee8128bb76c6d5da7daa8d34c25d61
notunrandom <122674938+notunrandom@users.noreply.github.com>
2026-08-13 13:16:00 +0000

    lmdb@0.9.36: add 0.9.36 bottle.

--- Formula/lmdb@0.9.36.rb
+++ Formula/lmdb@0.9.36.rb
@@ -6,6 +6,13 @@ class LmdbAT0936 < Formula
   license "OLDAP-2.8"
   version_scheme 1
 
+  bottle do
+    root_url "https://github.com/notunrandom/homebrew-cardano/releases/download/lmdb@0.9.36-0.9.36"
+    sha256 cellar: :any, arm64_tahoe:  "bd4522cd4eb922cc2356fbd2e4084d286969ea0d15109e876e69954df2daac6f"
+    sha256 cellar: :any, arm64_linux:  "4c56233a1079a3918219d5defe14a1d91be511e174a6fca62c52602ad45bedb7"
+    sha256 cellar: :any, x86_64_linux: "517c26adf986c658b97daf0b897146638c0077c90d44c1dcca1e052312464049"
+  end
+
   depends_on "pkgconf" => :test
 
   def install
