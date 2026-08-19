0b640792e9c4d0d211b52adbaddecd197d828bf2
notunrandom <122674938+notunrandom@users.noreply.github.com>
2026-08-19 13:54:35 +0200

    Ogmios formula: add a service
    Closes #38.
    
    Signed-off-by: notunrandom <122674938+notunrandom@users.noreply.github.com>

--- Formula/ogmios.rb
+++ Formula/ogmios.rb
@@ -5,19 +5,12 @@ class Ogmios < Formula
   version "7.0.0"
   license "MPL-2.0"
 
-  bottle do
-    root_url "https://github.com/notunrandom/homebrew-cardano/releases/download/ogmios-7.0.0"
-    sha256 cellar: :any, arm64_tahoe:   "85e31b80ea0d23ef608614e9bacc4af4517beeaffa4faea5bf1b905c252aa62a"
-    sha256 cellar: :any, arm64_sequoia: "81317b1d4a29a3494fa3bdc64b0a6c4fdb43bf296924c7b72c459d26a668cdf4"
-    sha256 cellar: :any, arm64_linux:   "473f228f526904e322d75c89195e78b24f27b7b7f9a57f65a74081821fe97099"
-    sha256 cellar: :any, x86_64_linux:  "14708e583eb34e31be7039584f811cba9ec9b4362ae49b997997a7cc8fab4448"
-  end
-
   depends_on "cabal-install" => :build
   depends_on "ghc@9.6" => :build
   depends_on "notunrandom/cardano/blst"
   depends_on "notunrandom/cardano/cardano-node"
   depends_on "notunrandom/cardano/libsodium-cardano"
+  depends_on "notunrandom/cardano/lmdb@0.9.36"
   depends_on "notunrandom/cardano/secp256k1@0.3.2"
   depends_on "pkgconf"
 
@@ -39,15 +32,48 @@ class Ogmios < Formula
     secp256k1 = Formula["notunrandom/cardano/secp256k1@0.3.2"]
     ENV.prepend_path "PKG_CONFIG_PATH", secp256k1.opt_lib/"pkgconfig"
 
+    lmdb = Formula["notunrandom/cardano/lmdb@0.9.36"]
+    ENV.prepend_path "PKG_CONFIG_PATH", lmdb.opt_lib/"pkgconfig"
+
     cd = src/"server"
     Dir.chdir(cd) do
       system "cabal", "update"
       system "cabal", "build", "ogmios:exe:ogmios"
       system "cabal", "v2-install", *std_cabal_v2_args, "ogmios:exe:ogmios"
     end
+    (etc/"cardano/ogmios/#{version}").mkpath
+    (var/"cardano/ogmios").mkpath
+    cardano_node_version = Formula["cardano-node"].version
+    (libexec/"ogmios-service.sh").write <<~SH
+      #!/bin/bash
+      NETWORK=preprod
+      NODESVCCONFIG="#{etc}/cardano/#{cardano_node_version}/cardano-node-service.conf"
+      if [[ -f "$NODESVCCONFIG" ]]; then
+        source "$NODESVCCONFIG"
+      fi
+      PORT=1337
+      HOST=127.0.0.1
+      SVCCONFIG="#{etc}/cardano/ogmios/#{version}/ogmios-service.conf"
+      if [[ -f "$SVCCONFIG" ]]; then
+        source "$SVCCONFIG"
+      fi
+      CONFIG="#{etc}/cardano/#{cardano_node_version}/$NETWORK/config.json"
+      SOCKET="#{var}/cardano/#{cardano_node_version}/$NETWORK/node.socket"
+      exec "#{bin}/ogmios" --host $HOST  --port $PORT --node-config $CONFIG --node-socket $SOCKET
+    SH
+    (libexec/"ogmios-service.sh").chmod 0755
+  end
+
+  service do
+    run [opt_libexec/"ogmios-service.sh"]
+    keep_alive true
+    log_path var/"cardano/ogmios/log"
+    error_log_path var/"cardano/ogmios/log"
   end
 
   test do
     system bin/"ogmios", "--version"
+    assert_path_exists etc/"cardano/ogmios/#{version}"
+    assert_path_exists var/"cardano/ogmios"
   end
 end
